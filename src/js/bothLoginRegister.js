f7d2c250abb1fea1a9e3c25f5d4333ac9340d444
ikarosyu <ikarosyu@gmail.com>
2019-07-05 09:28:31 +0800

    完成登陆注册功能

---
+++ src/js/bothLoginRegister.js
@@ -0,0 +1,68 @@
+require(['./config'], () => {
+    require(['jquery', 'loadHF'], ($) => {
+        // 登陆注册类
+        class LoginRegister {
+            constructor() {
+                this.addListener()
+            }
+
+            addListener() {
+                $(".button_reg").on("click", this.registerHandler.bind(this))
+                $(".login_btn").on("click", this.loginHandler)
+            }
+
+            registerHandler(e) {
+                e.preventDefault()
+                // 获取待提交的用户注册信息
+                const data = $(".form-register").serialize()
+                console.log(data)
+                // $.post('http://localhost/ShiseidoPC/api/register.php', data, (res) => {
+                //     console.log(res)
+                //     if (res.data.status === 1) { // 注册成功
+                        alert("注册成功！请登录")
+                //         this.showInfo()
+                //     }
+                //     else {
+                //         $('.register-error').removeClass('hidden').text(res.data.message)
+                //     }
+                // }, 'json')
+            }
+
+            //将用户信息，发送到登录表单中
+            showInfo() {
+                const objInfo = {}
+                const infoData = $(".form-register").serialize().split("&")
+                // console.log(infoData)
+                // 将数组传换成对象
+                infoData.forEach((val) => {
+                    let new_infoData = val.split("=")
+                    objInfo[new_infoData[0]] = new_infoData[1]
+                })
+
+                $(".loginUsername").val(objInfo.username)
+                $(".loginPassword").val(objInfo.password)
+            }
+
+            // 登录
+            loginHandler(e) {
+                e.preventDefault()
+                const data = $(".login_form").serialize()
+                $.post('http://localhost/ShiseidoPC/api/login.php', data, (res) => {
+                    if (res.data.status === 1) {
+
+                        $(".un_login").css({display: "none"})
+                        $(".al_login").css({display: "block"})
+                        alert("登录成功，快去购物吧！")
+                        location = 'http://localhost:1905/'
+
+
+                    }else {
+                        $('.login-error').removeClass('hidden').text(res.data.message)
+                    }
+                }, 'json')
+            }
+        }
+
+        new LoginRegister()
+    })
+})
